6576bb85790d7653cd8eed42dfd49d95d1bd15cb
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-01-02 09:14:23 +0200

    kainaSuPVM

---
+++ functions/sum.js
@@ -0,0 +1,21 @@
+function add(a, b){
+
+    return a + b
+}
+
+
+const s4 = add(7, 5)
+console.log(s4)
+
+
+function kainaSuPVM(kainaBePVM){
+    const PVM = 21;
+    const coof = 1 + PVM / 100;
+    const galutineKaina = kainaBePVM * coof;
+    return galutineKaina + ' Eur';
+
+}
+
+
+console.log(kainaSuPVM(100))
+console.log(kainaSuPVM(200))
